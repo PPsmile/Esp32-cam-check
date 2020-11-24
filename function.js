fd12e03e3f2983e942d98b05f9f2323ba4670363
PPsmile <p.fifa2012@gmail.com>
2020-11-24 13:05:07 +0700

    add post image

--- function.js
+++ function.js
@@ -1,26 +1,57 @@
-const userID = document.getElementById('userID')
-const firstName = document.getElementById('firstName')
-const lastName = document.getElementById('lastName')
-const age = document.getElementById('age')
-const addBtn = document.getElementById('addBtn')
-const updateBtn = document.getElementById('updateBtn')
-const removeBtn = document.getElementById('removeBtn')
-
+// const userID = document.getElementById('userID')
+// const firstName = document.getElementById('firstName')
+// const lastName = document.getElementById('lastName')
+// const age = document.getElementById('age')
+// const addBtn = document.getElementById('addBtn')
+// const updateBtn = document.getElementById('updateBtn')
+// const removeBtn = document.getElementById('removeBtn')
 const database = firebase.database();
 const rootRef = database.ref('esp32-cam')
-rootRef.once('value', function(snapshot){
-	console.log(snapshot.val());
-    snapshot.forEach(function(element){
-        document.querySelector('#picture').innerHTML += `
-			<div>${element.val()}</div>
-			<button class="btn btn-danger">Delete</button>`
-    });
+new Vue({
+	el: '#pic',
+	data:{
+
+	},
+
+	created(){
+		rootRef.once('value',snapshot =>{
+			snapshot.forEach(childSnapshot => {
+			var image = childSnapshot.child('photo').val();
+			console.log(childSnapshot.val());
+		
+			// $("#picture").append("<div><img id='myImg' src" + image + "></div>");
+			$('#picture').prepend("<div><img id='theImg' src=" + image + " /> </div>");
+			})
+		
+		})
+	}
+
 })
 
-removeBtn.addEventListener('click', e => {
-	e.preventDefault();
-	firebase.database().ref('esp32-cam/photo').remove();
-});
+
+
+
+
+
+
+
+
+
+// rootRef.once('value', function(snapshot){
+// 	console.log(snapshot.val());
+//     snapshot.forEach(function(element){
+//         document.querySelector('#picture').innerHTML += `
+// 			<div>${element.val()}</div>
+// 			<button class="btn btn-danger">Delete</button>`
+//     });
+// })
+
+// removeBtn.addEventListener('click', e => {
+// 	e.preventDefault();
+// 	database.ref('esp32-cam/photo').child(photo.value).remove()
+// });
+
+
 
 
 
